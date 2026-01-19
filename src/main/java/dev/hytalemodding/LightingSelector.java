4e2e38e4f68a3f5ca12b4aacf2db591bdabc7df9
nomadicbits <nomadicbits@gmail.com>
2026-01-19 12:19:15 -0500

    update config for simple mod

--- src/main/java/dev/hytalemodding/LightingSelector.java
+++ src/main/java/dev/hytalemodding/LightingSelector.java
@@ -8,9 +8,9 @@ import dev.hytalemodding.events.ExampleEvent;
 
 import javax.annotation.Nonnull;
 
-public class ExamplePlugin extends JavaPlugin {
+public class LightingSelector extends JavaPlugin {
 
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+    public LightingSelector(@Nonnull JavaPluginInit init) {
         super(init);
     }
 
